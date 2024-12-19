7ee4b30cf463410a44c68c618354ba279992d6bd
Valéria Carvalho <val.mar.dinis@gmail.com>
2024-12-19 14:47:06 +0000

    beautifull_plot

--- DATA/data_creation.py
+++ DATA/data_creation.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 import numpy as np
 
-M=np.linspace(1,2,100)
-R=np.linspace(10,14,100)
+x=np.linspace(0,1,100)
 
-pd.DataFrame([M,R],index=['M','R']).T.to_csv("./data.csv")
+y=np.cos(x)
+
+pd.DataFrame([x,y],index=['x','y']).T.to_csv("data/data.csv")
--- plot/ns2dview.py
+++ plot/ns2dview.py
@@ -1,11 +1,28 @@
 import numpy as np
 import matplotlib.pylab as plt
+import pandas as pd
+from scipy import interpolate
 
-def f(r, theta):
-    return r * np.cos(theta)
+
+dataset=pd.read_csv('./data/data.csv',index_col=0)
+
+
+# Lest imagine fro a minute that what we are seing is : we are fixing a maximum mass of M=1.4 and a massimum radius of R=12 and 
+# we are seing the pressure changing with the radius of the star 
 
 rlist=np.arange(0,1.01,0.01)
+
+interp=interpolate.interp1d(
+        x=dataset.iloc[:,0],
+        y=dataset.iloc[:,1],
+        kind=   'linear')
+
 thetalist=np.radians(np.arange(0,361,1))
+
+
+def f(r, theta):
+    return interp(r)
+
 rmesh, thetamesh = np.meshgrid(rlist, thetalist)
 
 FullFunction = f(rmesh, thetamesh)
