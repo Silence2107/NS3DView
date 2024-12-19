37ca000f74d8b2ca819d4fc2a4cadbb0fb8ea699
Valéria Carvalho <val.mar.dinis@gmail.com>
2024-12-19 15:35:53 +0000

    with argparse

--- plot/ns2dview.py
+++ plot/ns2dview.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import interpolate
+import argparse as arg
 
 
-dataset=pd.read_csv('./data/data.csv',index_col=0)
+parser = arg.ArgumentParser(description='Description of your program')
+parser.add_argument('-p','--pdf', help='Description for foo argument', default="temp.pdf")
+parser.add_argument('-c','--csv', help='Description for bar argument', default='./data/data.csv')
+args = parser.parse_args()
 
 
+dataset=pd.read_csv(args.csv,index_col=0)
 
 # Lest imagine fro a minute that what we are seing is : we are fixing a maximum mass of M=1.4 and a massimum radius of R=12 and 
 # we are seing the pressure changing with the radius of the star 
@@ -31,5 +36,5 @@ FullFunction = f(rmesh, thetamesh)
 fig, ax = plt.subplots(dpi=120,subplot_kw=dict(projection='polar'))
 ax.contourf(thetamesh, rmesh, FullFunction, 100, cmap='plasma')
 
-plt.savefig('NS_thing.pdf')
+plt.savefig(args.pdf)
 plt.show()
